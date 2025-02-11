e3df84c234afa8eed59655f3c9c1a00db1da4e15
jasmit428 <jasmit.kaur@edu.sait.ca>
2025-02-11 15:42:57 -0700

    edited page.js in week-4

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,7 +3,7 @@ import NewItem from './new-item';
 export default function Week4Page() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Week 4 Assignment: New Item Component</h1>
+      <h1 className="text-2xl font-bold mb-4">New Item Component</h1>
       <NewItem />
     </div>
   );
