2aca82ce0ed32916f07a8b474f415f36d9f0827d
jasmit428 <jasmit.kaur@edu.sait.ca>
2025-02-24 22:23:10 -0700

    Updated new-item.js and page.js

--- app/page.js
+++ app/page.js
@@ -1,22 +1,3 @@
-/*import Link from 'next/link';
-
-export default function Home() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-start pt-20 text-white">
-      <h1 className="text-4xl font-bold mb-6">
-        CPRG 306: Web Development 2 - Assignments
-      </h1>
-      
-      <div className="flex flex-col gap-2">
-        <Link href="/week-2" className="hover:text-green-400">
-          Week 2 Assignment
-        </Link>
-      </div>
-    </div>
-  );
-}
-  */
-
 import Link from 'next/link';
 
 export default function Home() {
@@ -35,6 +16,9 @@ export default function Home() {
         </Link>
         <Link href="/week-4" className="hover:text-green-400">
           Week 4 Assignment
+        </Link>
+        <Link href="/week-5" className="hover:text-green-400">
+          Week 5 Assignment
         </Link>
       </div>
     </div>
